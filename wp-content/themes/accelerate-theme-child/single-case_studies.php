e747ce88e1365f1ac227d3fc4dcc1b7123fb2ad2
TeniCola <teni.coker@gmail.com>
2018-07-29 15:11:27 -0400

    added ACF plugin, moved hard code to custom fields in case study post

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,12 +14,38 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
+<div id="primary" class="site-content sidebar">
+	<div class="main-content" role="main">
+		<?php while ( have_posts() ) : the_post();
+			$services = get_field('services');
+			$client = get_field('client');
+			$link = get_field('site_link');
+			$image_1 = get_field('image_1');
+			$image_2 = get_field('image_2');
+			$image_3 = get_field('image_3');  ?>
+
 				<article class="case-study">
-					<?php the_content(); ?>
+					<aside class="case-study-sidebar">
+						<h2><?php the_title(); ?></h2>
+						<h4><?php echo $services; ?></h4>
+						<h4><span>Client: <?php echo $client; ?></span></h4>
+						<?php the_content(); ?>
+						<p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site &rsaquo;</a></p>
+					</aside>
 				</article>
+
+				<div class="case-study-images">
+					<?php if($image_1) { ?>
+						<img src="<?php echo $image_1; ?>"/>
+					<?php } ?>
+					<?php if($image_2) { ?>
+						<img src="<?php echo $image_2; ?>"/>
+					<?php } ?>
+					<?php if($image_3) { ?>
+						<img src="<?php echo $image_3; ?>"/>
+					<?php } ?>
+				</div>
+
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
