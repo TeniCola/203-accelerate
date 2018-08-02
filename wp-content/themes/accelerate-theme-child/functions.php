740ab6d3556ffc252309f243a0a2130f9b06b676
TeniCola <teni.coker@gmail.com>
2018-08-02 19:35:33 -0400

    added contact page with ninja form plugin, adjusted styles

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -32,3 +32,14 @@ function create_custom_post_types() {
     );
 }
 add_action( 'init', 'create_custom_post_types' );
+
+// Create new class for contact-us page
+function accelerate_body_classes( $classes ) {
+
+ if ( is_page( 'contact-us' ) ) {
+   $classes[] = 'contact';
+ }
+    return $classes;
+}
+
+add_filter( 'body_class','accelerate_body_classes' );
